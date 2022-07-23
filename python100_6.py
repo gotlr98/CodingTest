c5b3d152db05e3f5c70d5978f87193773859df73
gotlr98 <gotlr98@naver.com>
2022-07-23 20:16:13 +0900

    feat : add question 52

--- python100_6.py
+++ python100_6.py
@@ -1,28 +1,50 @@
-# 문제 51 : merge sort를 만들어보자
+# # 문제 51 : merge sort를 만들어보자
 
-def merge_sort(input_list):
+# def merge_sort(input_list):
+#     list_len = len(input_list)
+#     if list_len <= 1:
+#         return input_list
+#     mid_idx = list_len // 2
+#     list_one = merge_sort(input_list[:mid_idx])
+#     list_two = merge_sort(input_list[mid_idx:])
+
+#     result = []
+
+#     while list_one and list_two:
+#         if list_one[0] < list_two[0]:
+#             result.append(list_one.pop(0))
+#         else:
+#             result.append(list_two.pop(0))
+    
+#     while list_one:
+#         result.append(list_one.pop(0))
+#     while list_two:
+#         result.append(list_two.pop(0))
+    
+#     return result
+
+# giv_list = [180, 145, 165, 45, 170, 175, 173, 171]
+
+# print(merge_sort(giv_list))
+
+# 문제 52 : quick sort
+
+def quick_sort(input_list):
     list_len = len(input_list)
     if list_len <= 1:
         return input_list
-    mid_idx = list_len // 2
-    list_one = merge_sort(input_list[:mid_idx])
-    list_two = merge_sort(input_list[mid_idx:])
 
-    result = []
+    stand = input_list.pop(list_len//2)
+    group_one = []
+    group_two = []
 
-    while list_one and list_two:
-        if list_one[0] < list_two[0]:
-            result.append(list_one.pop(0))
+    for i in range(list_len-1):
+        if input_list[i] < stand:
+            group_one.append(input_list[i])
         else:
-            result.append(list_two.pop(0))
-    
-    while list_one:
-        result.append(list_one.pop(0))
-    while list_two:
-        result.append(list_two.pop(0))
-    
-    return result
+            group_two.append(input_list[i])
+    return quick_sort(group_one) + [stand] + quick_sort(group_two)
+list = input().split(' ')
+list = [int(i) for i in list]
 
-giv_list = [180, 145, 165, 45, 170, 175, 173, 171]
-
-print(merge_sort(giv_list))
+print(quick_sort(list))
