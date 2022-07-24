799ffd8a51bb332e2e4ed801f51c5858dc3b5525
gotlr98 <gotlr98@naver.com>
2022-07-24 14:36:12 +0900

    feat : add question 53

--- python100_6.py
+++ python100_6.py
@@ -1,31 +1,31 @@
-# # 문제 51 : merge sort를 만들어보자
+# 문제 51 : merge sort를 만들어보자
 
-# def merge_sort(input_list):
-#     list_len = len(input_list)
-#     if list_len <= 1:
-#         return input_list
-#     mid_idx = list_len // 2
-#     list_one = merge_sort(input_list[:mid_idx])
-#     list_two = merge_sort(input_list[mid_idx:])
+def merge_sort(input_list):
+    list_len = len(input_list)
+    if list_len <= 1:
+        return input_list
+    mid_idx = list_len // 2
+    list_one = merge_sort(input_list[:mid_idx])
+    list_two = merge_sort(input_list[mid_idx:])
 
-#     result = []
+    result = []
 
-#     while list_one and list_two:
-#         if list_one[0] < list_two[0]:
-#             result.append(list_one.pop(0))
-#         else:
-#             result.append(list_two.pop(0))
+    while list_one and list_two:
+        if list_one[0] < list_two[0]:
+            result.append(list_one.pop(0))
+        else:
+            result.append(list_two.pop(0))
     
-#     while list_one:
-#         result.append(list_one.pop(0))
-#     while list_two:
-#         result.append(list_two.pop(0))
+    while list_one:
+        result.append(list_one.pop(0))
+    while list_two:
+        result.append(list_two.pop(0))
     
-#     return result
+    return result
 
-# giv_list = [180, 145, 165, 45, 170, 175, 173, 171]
+giv_list = [180, 145, 165, 45, 170, 175, 173, 171]
 
-# print(merge_sort(giv_list))
+print(merge_sort(giv_list))
 
 # 문제 52 : quick sort
 
@@ -48,3 +48,19 @@ list = input().split(' ')
 list = [int(i) for i in list]
 
 print(quick_sort(list))
+
+# 문제 53 : 괄호 문자열
+
+def check_str(a):
+    # print(a[::-1])
+    len_a = len(a)
+    if a[0] == ')':
+        return 'NO'
+    elif a[len_a-1] == '(':
+        return 'NO'
+    elif a.count('(') != a.count(')'):
+        return 'NO'
+    return 'YES'
+
+a = input()
+print(check_str(a))
