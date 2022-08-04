02071ca6e5db5676917d85e55481196651867bcd
gotlr98 <gotlr98@naver.com>
2022-08-04 09:58:50 +0900

    feat : add question 60

--- python100_6.py
+++ python100_6.py
@@ -147,3 +147,11 @@ for i in range(50):
         k+=1
     else:
         print('=', end='')
+
+# 문제 60 : enumerate
+
+student = ['강은지','김유정','박현서','최성훈','홍유진','박지호','권윤일','김채리','한지호',
+          '김진이','김민호','강채연']
+student.sort()
+for num, name in enumerate(student):
+    print('번호: ' + str(num) + ', ' + '이름:' + ' '+ name)
