0e9681f2837fbfc200ec16887f7d980bd74f1d07
gotlr98 <gotlr98@naver.com>
2022-08-08 13:18:14 +0900

    feat : solve question 61

--- python100_7.py
+++ python100_7.py
@@ -3,3 +3,18 @@
 # 출력 : a3b4c1d4
 
 a = input()
+output = ''
+prev = a[0]
+count = 1
+for i in range(1, len(a)):
+    
+    if prev != a[i]:
+        output =  output + (a[i-1] + str(count))
+        count = 0
+    
+    count += 1
+    prev = a[i]
+    if i == len(a)-1:
+        output = output + (a[i-1] + str(count))
+    
+print(output)
