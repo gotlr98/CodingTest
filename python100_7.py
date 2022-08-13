77d96e144741ca2efc73642e4ae4239b9e9ad5b0
gotlr98 <gotlr98@naver.com>
2022-08-13 21:38:50 +0900

    Update python100_7.py

--- python100_7.py
+++ python100_7.py
@@ -1,39 +1,39 @@
-# # 문제 61 : 문자열 압축하기
-# # aaabbbbcdddd
-# # 출력 : a3b4c1d4
+# 문제 61 : 문자열 압축하기
+# aaabbbbcdddd
+# 출력 : a3b4c1d4
 
-# a = input()
-# output = ''
-# prev = a[0]
-# count = 1
-# for i in range(1, len(a)):
+a = input()
+output = ''
+prev = a[0]
+count = 1
+for i in range(1, len(a)):
     
-#     if prev != a[i]:
-#         output =  output + (a[i-1] + str(count))
-#         count = 0
+    if prev != a[i]:
+        output =  output + (a[i-1] + str(count))
+        count = 0
     
-#     count += 1
-#     prev = a[i]
-#     if i == len(a)-1:
-#         output = output + (a[i-1] + str(count))
+    count += 1
+    prev = a[i]
+    if i == len(a)-1:
+        output = output + (a[i-1] + str(count))
     
-# print(output)
+print(output)
 
-# # 문제 62 : 20190923 출력
-# # 코드 내에 숫자가 없어야 한다.
-# # 파일 이름이나 경로를 사용해서는 안된다.
-# # 시간, 날짜 함수를 사용해서는 안된다
-# # 에러 번호 출력을 이용해서는 안된다.
-# # input을 이용해서는 안된다
+# 문제 62 : 20190923 출력
+# 코드 내에 숫자가 없어야 한다.
+# 파일 이름이나 경로를 사용해서는 안된다.
+# 시간, 날짜 함수를 사용해서는 안된다
+# 에러 번호 출력을 이용해서는 안된다.
+# input을 이용해서는 안된다
 
 
-# # 문제 63 : 친해지고 싶어
-# # 입력이 주어지면 앞글자만 줄여 출력하기
+# 문제 63 : 친해지고 싶어
+# 입력이 주어지면 앞글자만 줄여 출력하기
 
-# string = input().split()
+string = input().split()
 
-# for i in string:
-#     print(i[0], end='')
+for i in string:
+    print(i[0], end='')
 
 # 문제 64 : 이상한 엘레베이터
 # 정량 N에 정확히 맞춰야만 움직이는 화물용 엘레베이터가 있다
