cf6fee994b87ad1c21975fc858cbacb046224112
gotlr98 <gotlr98@naver.com>
2022-08-14 11:52:32 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
-num = int(input())
-b = []
-while True:
-    if num / 10 < 1:
-        b.append(num)
-        break
+# num = int(input())
+# b = []
+# while True:
+#     if num / 10 < 1:
+#         b.append(num)
+#         break
     
-    b.append(num % 10)
-    num = num // 10
+#     b.append(num % 10)
+#     num = num // 10
     
-print(sum(b))
+# print(sum(b))
+
+print(int('a'))
