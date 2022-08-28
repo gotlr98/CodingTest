d9f0b1cb152a4efd66d62e7a83f9759ab778d6c7
gotlr98 <gotlr98@naver.com>
2022-08-28 15:57:50 +0900

    Update python100_7.py

--- python100_7.py
+++ python100_7.py
@@ -90,8 +90,10 @@ rule = input()
 conv = []
 for t in top:
     for a in t:
+        count = 0
         # print(a)
         if a in rule:
+            count += 1
             conv.append(a)
 print(conv)
 
