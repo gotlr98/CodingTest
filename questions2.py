dca44f95e580c3686a941dd52c4bfa1064404a63
gotlr98 <gotlr98@naver.com>
2022-09-25 11:28:52 +0900

    Create questions2.py

---
+++ questions2.py
@@ -0,0 +1,23 @@
+from re import L
+
+
+def solution(users, emoticons):
+    answer = []
+    return answer
+
+users = [[40, 10000], [25, 10000]]
+emoticons = [7000, 9000]
+users_conv = {}
+emoticons_conv = {}
+
+for i in range(len(users)):
+    users_conv[i+1] = users[i]
+for j in range(len(emoticons)):
+    for k in users_conv:
+        if j+1 in emoticons_conv:
+            emoticons_conv[j+1]+=(int(users_conv[k][0]) / 100 * emoticons[j+1])
+        else:
+            emoticons_conv[j+1] = int(users_conv[k][0]) / 100 * emoticons[j]
+
+
+print(emoticons_conv)
