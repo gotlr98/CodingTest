92d1c07b1978e8930cfced4b67bb4f4ca6fa3908
gotlr98 <gotlr98@naver.com>
2022-10-11 14:19:00 +0900

    Update python100_8.py

--- python100_8.py
+++ python100_8.py
@@ -1,3 +1,26 @@
 # 문제 71
 # 깊이 우선 탐색 - 빈칸을 채워 코드를 완성하시오
 
+graph = {
+        'A': set(['B', 'C', 'E']),
+        'B': set(['A']),
+        'C': set(['A']),
+        'D': set(['E', 'F']),
+        'E': set(['A', 'D']),
+        'F': set(['D'])
+}
+
+def dfs(graph, start):
+    visited = []
+    stack = [start]
+
+    while stack:
+        n = stack.pop()
+        if n not in visited:
+            visited.append(n)
+            while graph[n]:
+                
+
+    return visited
+
+print(dfs(graph, 'E'))
