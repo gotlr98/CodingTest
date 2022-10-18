1608c36029c49ac60393f2fb871709a0274bae23
gotlr98 <gotlr98@naver.com>
2022-10-18 19:28:27 +0900

    Update programmers_level1.py

--- programmers_level1.py
+++ programmers_level1.py
@@ -37,29 +37,56 @@
 
 # 신고 결과 받기
 
-def solution(id_list, report, k):
-    answer = []
-    for i in range(len(id_list)):
-        answer.append(0)
+# def solution(id_list, report, k):
+#     answer = []
+#     for i in range(len(id_list)):
+#         answer.append(0)
     
-    report_list = {}
-    for i in id_list:
-        report_list[i] = 0
+#     report_list = {}
+#     for i in id_list:
+#         report_list[i] = 0
 
-    report_history = []
-    for i in set(report):
-        report_list[i.split(' ')[1]] += 1
-        report_history.append(i)
+#     report_history = []
+#     for i in set(report):
+#         report_list[i.split(' ')[1]] += 1
+#         report_history.append(i)
 
 
-    report_name = []
+#     report_name = []
 
-    for i in report_list:
-        if report_list[i] >= k:
-            report_name.append(i)
+#     for i in report_list:
+#         if report_list[i] >= k:
+#             report_name.append(i)
 
-    for j in report_history:
-        if j.split(' ')[1] in report_name:
-            answer[id_list.index(j.split(' ')[0])] += 1
-    return answer
+#     for j in report_history:
+#         if j.split(' ')[1] in report_name:
+#             answer[id_list.index(j.split(' ')[0])] += 1
+#     return answer
 
+# 숫자 짝꿍
+
+def solution(X, Y):
+    answer = ''
+
+    mid = ''
+    count = 0
+    for i in X:
+        for j in Y:
+            if i==j:
+                mid += j
+                Y = Y[:count] + Y[count+1:]
+                print(Y)
+            count += 1
+        count = 0
+    a = list(mid)
+    for k in range(len(mid)):
+        answer += max(a)
+        a.remove(max(a))
+    
+    if answer == '':
+        answer = '-1'
+    answer = int(answer)
+    
+    return str(answer)
+
+solution("5525", "1255")
