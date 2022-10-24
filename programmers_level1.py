fa39c149f62d9b09f820e940d61ef2799600df45
gotlr98 <gotlr98@naver.com>
2022-10-24 16:55:18 +0900

    Update programmers_level1.py

--- programmers_level1.py
+++ programmers_level1.py
@@ -216,4 +216,22 @@
 #         sum += x
 #     return answer
 
-# 
+# 콜라츠 추측
+
+def solution(num):
+    answer = 0
+    
+    while True:
+        if num == 1:
+            return 0
+        else:
+            if num % 2 == 0:
+                num /= 2
+            else:
+                num = num * 3 + 1
+            answer += 1
+
+            if answer > 500:
+                return -1
+            elif num == 1:
+                return answer
