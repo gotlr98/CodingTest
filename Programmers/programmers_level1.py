f58f9909d48c3b137ca2cd507608d25c17bfb7fe
gotlr98 <gotlr98@naver.com>
2022-10-27 14:33:36 +0900

    Update programmers_level1.py

--- Programmers/programmers_level1.py
+++ Programmers/programmers_level1.py
@@ -393,3 +393,15 @@
 #         count = 0
 
 #     return sum(list)
+
+# 행렬의 덧셈
+
+def solution(arr1, arr2):
+    answer = []
+    
+    for i in range(len(arr1)):
+        sum = []
+        for j in range(len(arr1[i])):
+            sum.append(arr1[i][j] + arr2[i][j])
+        answer.append(sum)
+    return answer
