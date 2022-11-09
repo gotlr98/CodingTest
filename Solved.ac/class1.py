84276102dfc4b5b843ef2c2a9587f6764bd47188
gotlr98 <gotlr98@naver.com>
2022-11-09 12:38:05 +0900

    Update class1.py

--- Solved.ac/class1.py
+++ Solved.ac/class1.py
@@ -1,4 +1,24 @@
 # 단어의 개수
 
-sentence = input()
-print(len((sentence.strip()).split(' ')))
+# sentence = input()
+# if sentence == ' ':
+#     print(0)
+# else:
+#     print(len((sentence.strip()).split(' ')))
+
+# 단어 공부
+
+# word = input().upper()
+
+# count = {}
+# for i in word:
+#     if i in count:
+#         count[i] += 1
+#     else:
+#         count[i] = 1
+# max_val = [k for k,v in count.items() if max(count.values()) == v]
+# if len(max_val) > 1:
+#     print('?')
+# else:
+#     print(max_val[0])
+
