c898c0f4ecca57577e8106e733862b6ce3878e47
gotlr98 <gotlr98@naver.com>
2022-12-04 15:16:03 +0900

    Update programmers_level1.py

--- Programmers/programmers_level1.py
+++ Programmers/programmers_level1.py
@@ -793,23 +793,37 @@
 
 # 소수 만들기
 
-import math
+# import math
 
-def primenumber(n):
+# def primenumber(n):
     
-    for i in range(2, int(math.sqrt(n))+1):
-        if n % i == 0:
-            return False
-    return True
+#     for i in range(2, int(math.sqrt(n))+1):
+#         if n % i == 0:
+#             return False
+#     return True
 
-def solution(nums):
+# def solution(nums):
+#     answer = 0
+    
+#     for i in range(len(nums)-2):
+#         for j in range(i+1, len(nums)-1):
+#             for k in range(j+1, len(nums)):
+#                 if primenumber(nums[i]+nums[j]+nums[k]):
+#                     answer += 1
+
+
+#     return answer
+
+# 콜라 문제
+
+def solution(a, b, n):
     answer = 0
-    
-    for i in range(len(nums)-2):
-        for j in range(i+1, len(nums)-1):
-            for k in range(j+1, len(nums)):
-                if primenumber(nums[i]+nums[j]+nums[k]):
-                    answer += 1
 
+    while n <= 0:
+
+        answer += n // a
+        n = n - n//a
 
     return answer
+
+print(solution(2, 1, 20))
